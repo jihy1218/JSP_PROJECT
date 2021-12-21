7a1a2183ab73a5cde73159d93e284745b73fb610
505 <505@DESKTOP-8GQP9MO>
2021-12-21 18:00:17 +0900

    2021.12.21

---
+++ jsp_chatting_project/src/main/java/dao/BoardDao.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import dto.Board;
+
+public class BoardDao extends DB {
+	private PreparedStatement preparedStatement;
+	private Connection connection;
+	private ResultSet resultSet;
+	
+	public static BoardDao boardDao = new BoardDao();
+	public static BoardDao getboardDao() {return boardDao;}
+	
+	// 게시물 작성
+	public boolean boardwrite(Board board) {
+		String sql="insert into board(b_title, b_contents, b_file, m_no) values(?,?,?,?)";
+		try {
+			preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.setString(1, board.getB_title());
+			preparedStatement.setString(2, board.getB_contents());
+			preparedStatement.setString(3, board.getB_file());
+			preparedStatement.setInt(4, board.getM_no());
+			preparedStatement.executeQuery();
+			return true;
+		} catch (Exception e) {System.out.println("게시물 작성오류");} return false;
+	}
+	
+	// 모든 게시물 출력
+	public ArrayList<Board> boardlist(int startrow, int listsize, String key, String keyword) {
+		ArrayList<Board> boards =new ArrayList<Board>();
+		
+		String sql=null;
+		
+		if(key==null&keyword==null) {
+			sql="select * from board order by b_no DESC limit ?, ?";
+		} else {
+			if(key.equals("b_writer")) {
+				
+			}
+		}
+	}
+}
