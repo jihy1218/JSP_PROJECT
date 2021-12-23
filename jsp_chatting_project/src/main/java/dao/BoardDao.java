f9f166f3c221f1d219f0076dd86abbbbbd033895
505 <505@DESKTOP-P96TS0E>
2021-12-23 18:01:04 +0900

    게시판 페이징 거의  검색 절반

--- jsp_chatting_project/src/main/java/dao/BoardDao.java
+++ jsp_chatting_project/src/main/java/dao/BoardDao.java
@@ -22,32 +22,80 @@ public class BoardDao extends DB {
 		} catch (Exception e) {System.out.println("게시물 작성오류");} return false;
 	}
 	
+//	// 모든 게시물 출력
+//
+//	public ArrayList<Board> boardlist(){
+//	    ArrayList<Board>boards= new ArrayList<Board>();
+//	    String sql = "select * from board order by b_no DESC";
+//	    try {
+//		preparedStatement = connection.prepareStatement(sql);
+//		resultSet = preparedStatement.executeQuery();
+//		while(resultSet.next()) {
+//		    Board board2 = new Board(
+//			    resultSet.getInt(1),
+//			    resultSet.getString(2),
+//			    resultSet.getString(3),
+//			    resultSet.getString(4),
+//			    resultSet.getInt(5),
+//			    resultSet.getString(6),
+//			    resultSet.getInt(7),
+//			    resultSet.getInt(8));
+//		    boards.add(board2);
+//			   
+//		}
+//		return boards;
+//	    } catch (Exception e) { 
+//		System.out.println(e);
+//	    }
+//	    return null;  
+//	}
+	
 	// 모든 게시물 출력
 
-	public ArrayList<Board> boardlist(){
-	    ArrayList<Board>boards= new ArrayList<Board>();
-	    String sql = "select * from board order by b_no DESC";
-	    try {
-		preparedStatement = connection.prepareStatement(sql);
-		resultSet = preparedStatement.executeQuery();
-		while(resultSet.next()) {
-		    Board board2 = new Board(
-			    resultSet.getInt(1),
-			    resultSet.getString(2),
-			    resultSet.getString(3),
-			    resultSet.getString(4),
-			    resultSet.getInt(5),
-			    resultSet.getString(6),
-			    resultSet.getInt(7),
-			    resultSet.getInt(8));
-		    boards.add(board2);
-			   
+		public ArrayList<Board> boardlist(int startrow, int listsize, String key, String keyword){
+		    ArrayList<Board>boards= new ArrayList<Board>();
+		    String sql = null;
+		    if(key ==null && keyword ==null) {// 검색이 없을경우
+			sql = "select* from board order by b_no DESC limit ?, ?";
+		    }else {// 검색이 있을경우
+			if(key.equals("m_no")) {
+			    int m_no = MemberDao.getmMemberDao().getm_no(keyword);
+			    sql = "select * from board where m_num ="+m_no+" order by b_no desc limit ? , ?";
+			}else if(key.equals("b_no")) {// 반호 검색 일치한 값만 검색
+			    sql ="select * from board where b_num ="+ keyword+ "order by b_bnum desc limit ? , ?";
+			}else {									//제목 혹은 내용 검색 : 포함된 값검색
+			    sql ="select * from board where "+key+"like'%"+keyword+"%' order by b_no desc limit ? , ?";
+			}
+		    }
+		    try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, startrow); preparedStatement.setInt(2, listsize);
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()) {
+			    Board board = new Board(
+				    resultSet.getInt(1),
+				    resultSet.getString(2),
+				    resultSet.getString(3),
+				    resultSet.getString(4),
+				    resultSet.getInt(5),
+				    resultSet.getString(6),
+				    resultSet.getInt(7),
+				    resultSet.getInt(8));
+				boards.add(board);
+			}
+			return boards;
+		    } catch (Exception e) {System.out.println(e);}
+			
+		    return null;
+		    
 		}
-		return boards;
-	    } catch (Exception e) { 
-		System.out.println(e);
+	//게시물 총개수 반환 메소드
+	public int boardcount(String key, String keyword) {
+	    String sql = null;
+	    if(key !=null && keyword !=null) {//검색이 있을떄 [조건 레코드 개수세기]
+		
 	    }
-	    return null;  
+	    return 0;
 	}
 
 	
--- jsp_chatting_project/src/main/webapp/carrot/view/board/boardlist.jsp
+++ jsp_chatting_project/src/main/webapp/carrot/view/board/boardlist.jsp
@@ -16,7 +16,24 @@
 		String key =request.getParameter("key");
 		String keyword = request.getParameter("keyword");
 		
-		ArrayList<Board> boards = BoardDao.getboardDao().boardlist(); 
+		//페이지 처리
+		String pagenum = request.getParameter("pagenum"); // 클릭한 페이지번호
+			if(pagenum==null){// 클릭한 페이지번호가 없으면 [게시판 첫화면]
+			    pagenum="1"; // 고것을 1페이지로 설정
+			}
+		int lastrow =BoardDao.getboardDao().boardcount(key, keyword); // 총게시물수
+		int listsize = 10; //페이지당 화면에 표시될 게시물수
+
+		int lastpage =0; //3. 마지막 페이지
+		if(lastrow%listsize==0){ // 만약에 총게시물 페이지당 게시물 나머지가 없으면 
+		    lastpage=lastrow/listsize;	// 총게시물 페이지당 게시물
+		}else{
+		    lastpage=lastrow/listsize+1;// 총게시물 페이지당 게시물~!~!
+		}
+		int currentpage = Integer.parseInt(pagenum); // 현재 페이지 번호 입니다
+		int startrow =(currentpage-1)*listsize;// 현재 페이지의 시작번호 입니다
+		// 등록 시 
+		ArrayList<Board> boards = BoardDao.getboardDao().boardlist(startrow, listsize, key, keyword);
 	%>
 	
 	<div class=container>
@@ -25,7 +42,10 @@
 		<div class="row">
 			<button class="mr-2 ml-3 btn btn-outline-danger"> 전체글 </button>		
 			<button class="btn btn-outline-success"> 인기글 </button>
+			<a href="boardwrite.jsp" ><button style="background-color:green;">글쓰기</button></a>
+		
 		</div><br>
+		
 		
 	<table class="table">
 		<tr style="border-top: solid 2px navy; border-bottom: solid 1.5px navy;">
@@ -48,9 +68,48 @@
 			</tr>
 		
 		<%} %>
-		
+	
 	</table>
-	</div>
+	<!-- 페이징 시작 -->
+		<div class="row">
+			<div class ="col-md-4 offset-4 my-3">
+			<ul class="pagination">
+					<% if( currentpage == 1){ %>
+					<%if(keyword==null){ %>
+					<li class="page-item"><a href="boardlist.jsp?pagenum=<%=currentpage%>&key=<%=key %>&keyword=<%=keyword %>" class="page-link"> 이전 </a> </li>
+					<%}else{%>
+					<li class="page-item"><a href="boardlist.jsp?pagenum=<%=currentpage%>" class="page-link"> 이전 </a> </li>	
+					<%} %>
+				<%}else{ %>	
+					<li class ="page-item"><a href="boardlist.jsp?pagenum=<%=currentpage-1%>&key=<%=key %>&keyword=<%=keyword %>" class="page-link"></a></li>
+				<%} %>		
+				
+				<!-- 게시물 의 수만큼 페이지 번호를 생성합니다 -->
+				<%for(int i =1 ; i<=5; i++) {%>
+					<li class="page-item"><a href="boardlist.jsp?pagenum=<%=i%>&key=<%=key%>&keyword=<%=keyword%>" class="page-link"><%=i %></a></li>
+				<%} %>		
+				<!-- 마지막 페이지에서 다음버튼 눌렀을떄 마지막페이를 고정해야만해오 -->
+				<% if(currentpage == lastpage) {%>
+					<li class ="page-item"><a href="boardlist.jsp?pagenum=<%=currentpage%>&key=<%=key%>&keyword=<%=keyword%>"class="page-link">다음</a></li>
+				<%}else{ %>
+					<li class="page-item"><a href="boardlist.jsp?pagenum=<%=currentpage+1 %>&key=<%=key %>&keyword=<%=keyword %>" class="page-link"> 다음 </a> </li>	
+				<%} %>
+			</ul>
+			</div>
+		</div>
+	<!-- 검색 -->
+	<form action ="boardlist.jsp?pagenum=<%=currentpage %>"method="get" class="col-md-5 offset-3 input-group my-3">
+		<select class="custom-select col-cmd-3" name="key">
+			<option value="b_title">제목</option>
+			<option value="b_no">번호</option>
+			<option value="m_no">작성자</option>
+		</select>
+		<input type="text" class="form-control" name="keyword"><!-- keyword 검색대상~! -->
+		<input type="submit"class=btn-outline-info value="검색">
+	</form>
+</div>
+	
+	
 	
 	
 </body>
