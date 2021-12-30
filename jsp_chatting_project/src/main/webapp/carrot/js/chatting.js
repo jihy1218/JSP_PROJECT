75281048a85c82248852ab8c191f3d4dd5aecf63
minwook <minwook@14.36.230.109>
2021-12-30 15:07:31 +0900

    1234

--- jsp_chatting_project/src/main/webapp/carrot/js/chatting.js
+++ jsp_chatting_project/src/main/webapp/carrot/js/chatting.js
@@ -153,8 +153,28 @@ function makeroom(){
 	}
 	
 
+	//채팅 파일
+	function chattingfile(){
+		
+		
+	var formData = new FormData($('#fileForm')[0]);
 
-				
+	alert(formData);
+		
+	$.ajax({ 
+		type: "POST",
+		enctype: 'multipart/form-data',
+	 	url: '../../controller/chattingimgcontroller.jsp',
+	 	data: {
+			formData : formData
+		},
+		processData: false,
+		contentType: false
+
+		});
+
+	}
+
 
 
 
